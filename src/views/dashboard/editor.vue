e7e4caee85914ea509e1671a72ebd757050c8fd8
chenxiaobin <604113530@qq.com>
2019-07-08 14:11:17 +0800

    迁入jmeditor

--- src/views/dashboard/editor.vue
+++ src/views/dashboard/editor.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
-    hhh
+    <div id="content1" contentEditable="true" class="editDemo">jhh</div>
+    <!-- <div class="mathquill-editable"></div> -->
   </div>
 </template>
 <script>
-import '@/JMEditor/jmeditor/mathquill-0.9.1/mathquill.css'
 export default {
-  name: 'editor',
-  data () {
+  data() {
     return {
-      test: ''
+      $: null, // jquery
+      editor: null // editor实例
+    }
+  },
+  async mounted() {
+    const { editor, jquery } = await this.getEditor()
+    // editor.disableAutoInline = true
+    editor.inline('content1')
+    this.editor = editor
+    this.$ = jquery
+    console.log(this.$.find('#content1'))
+  },
+  methods: {
+    getEditor() {
+      return new Promise(resolve => {
+        const inter = setInterval(() => {
+          if (window.JMEditor && window.JMEditor.ckEditor && window.$) {
+            resolve({ editor: window.JMEditor.ckEditor, jquery: window.$ })
+            clearInterval(inter)
+          }
+        }, 500)
+      })
     }
   }
 }
 </script>
+
+<style lang="scss">
+  .cke{
+    box-sizing: content-box;
+  }
+</style>
